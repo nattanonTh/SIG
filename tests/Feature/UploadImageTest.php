78c4217952fccff39a4dca87cc90f9acddeeb457
Nattanon Thamrongsup <>
2019-11-08 15:35:35 +0700

    Testing

--- tests/Feature/UploadImageTest.php
+++ tests/Feature/UploadImageTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use ImageSeeder;
 use Tests\TestCase;
 
@@ -17,7 +19,7 @@ class UploadImage extends TestCase
      * @return void
      */
     /** @test */
-    public function testOverviewPage()
+    public function overviewPage()
     {
         $this->seed(ImageSeeder::class);
         $user = factory(User::class)->create();
@@ -25,6 +27,24 @@ class UploadImage extends TestCase
         $response = $this->get('/');
         $response->assertStatus(200);
         $response->assertSee('600,000 B');
+
+    }
+
+    /** @test */
+    public function uploadImage(Type $var = null)
+    {
+        $this->seed(ImageSeeder::class);
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+        $file = UploadedFile::fake()->image('avatar.jpg');
+
+        $response = $this->json('POST', '/gallery/upload', [
+            'file' => $file
+        ]);
+        // Assert the file was stored...
+        Storage::disk('local')->assertExists('/uploads/' . $file->hashName());
+        // remove file
+        Storage::disk('local')->delete('/uploads/' . $file->hashName());
 
     }
 }
